66412cb9b01149b69bc3609d7c8148ac3bca4649
Frank Umpierrez <fcojaviermu@gmail.com>
2018-06-06 13:11:03 +0100

    A barebones NativeScript with the VueJS template to illustrate a bug

---
+++ src/main.js
@@ -0,0 +1,20 @@
+import Vue from 'nativescript-vue';
+
+import router from './router';
+
+import store from './store';
+
+import './styles.scss';
+
+// Uncommment the following to see NativeScript-Vue output logs
+//Vue.config.silent = false;
+var application = require("application");
+
+
+new Vue({
+
+  router,
+
+  store,
+
+}).$start();
